faf92b84da5297dc549715a74f2f24fd3f95325b
tushal012 <tushalkashyap11@gmail.com>
2021-05-09 17:22:12 +0530

    multiple and addition

--- testArithmetic.sh
+++ testArithmetic.sh
@@ -1,5 +1,10 @@
 #! /bin/bash -x
-
+a=10
+b=20
+c=30
+echo $a 
+echo $b 
+echo $c
 function addMul(){
 	local a=$1
 	local b=$2
@@ -7,8 +12,16 @@ function addMul(){
 	local val=$(( $a + $b * $c ))
 	echo $val
 }
-a=10
-b=20
-c=30
+function mulAdd(){
+        local a=$1
+        local b=$2
+        local c=$3
+        local val=$(( $a * $b + $c ))
+        echo $val
+}
+
+
 result="$(addMul $a $b $c)"
 echo $result
+resultMul="$(mulAdd $a $b $c)"
+echo $resultMul
